8bdea7eba18f88b2dbd4cc309dd142abe5429851
Taylor <taylort3450@proton.me>
2026-08-16 13:33:27 -0500

    Add event system to Agent class

--- src/rl_learning/agent/agent.py
+++ src/rl_learning/agent/agent.py
@@ -1,8 +1,23 @@
 from ..env import Env
+from ..events import EventEmitter, EventType
 from .brain import Brain
 
 
 class Agent:
-    def __init__(self, brain: Brain, env: Env):
+    def __init__(self, brain: Brain, env: Env, events: EventEmitter):
         self.brain = brain
         self.env = env
+        self.events = events
+        self.win: bool = False
+
+        self.events.subscribe(EventType.WIN, self.set_win)
+
+    def run_actions(self):
+        for direction in self.brain.actions:
+            self.env.move(direction)
+
+    def set_win(self):
+        self.win = True
+
+    def get_win_distance(self):
+        self.env
