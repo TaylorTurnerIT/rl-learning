b47a7b3e608000ba5cb9de17531d3a4d78fc4b0c
Taylor <taylort3450@proton.me>
2026-08-16 18:00:53 -0500

    Add > operator to Fitness dataclass

--- src/rl_learning/agent/fitness.py
+++ src/rl_learning/agent/fitness.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 
 
 @dataclass
@@ -6,3 +7,8 @@ class Fitness:
     win: bool
     distance_to_win: int
     move_count: int
+
+    def __gt__(self, other: Self) -> bool:
+        if self.win and not other.win:
+            return True
+        return self.distance_to_win > other.distance_to_win
