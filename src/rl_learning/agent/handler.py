31e3b84ba3b4126e8d19eb8fe177acfe826bef82
Taylor <taylort3450@proton.me>
2026-08-16 13:20:59 -0500

    Remove blank line before comment in AgentHandler init

--- src/rl_learning/agent/handler.py
+++ src/rl_learning/agent/handler.py
@@ -4,8 +4,9 @@ from .brain import Brain
 
 class AgentHandler:
     def __init__(self, env: Env, population: int, brain: Brain | None = None):
-
         # Assign the previous best or generate new brain
         self.brain = brain
         if self.brain is None:
             self.brain = Brain()
+
+        
