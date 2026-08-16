9a51342d4ee179d3df29d074b801c04975dabeee
Taylor <taylort3450@proton.me>
2026-08-16 14:18:16 -0500

    Refactor AgentHandler to manage agent populations

--- src/rl_learning/agent/handler.py
+++ src/rl_learning/agent/handler.py
@@ -1,12 +1,18 @@
 from ..env import Env
+from .agent import Agent
 from .brain import Brain
 
 
 class AgentHandler:
-    def __init__(self, env: Env, population: int, brain: Brain | None = None):
+    def __init__(self, population: int):
         # Assign the previous best or generate new brain
-        self.brain = brain
-        if self.brain is None:
-            self.brain = Brain()
+        self.brain = Brain()
+        self.population: int = population
+        self.agents: list[Agent] = []
+        self.best_agent: Agent | None = None
 
-        
+    def create_agents(self, brain: Brain):
+        if self.best_agent is not None:
+            self.brain = self.best_agent.brain
+        for _ in range(self.population):
+            self.agents.append(Agent(self.brain, ))
