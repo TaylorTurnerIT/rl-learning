5857d6175c29d8d80254fabf925f0d8fdfce5703
Taylor <taylort3450@proton.me>
2026-08-16 14:56:46 -0500

    Fix constructor parameter and env builder usage in AgentHandler

--- src/rl_learning/agent/handler.py
+++ src/rl_learning/agent/handler.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from logging import Logger
 
-from ..env import Env, EnvBuilder
+from ..env import Env, EnvBuilder, EnvParams
 from .agent import Agent
 from .brain import Brain
 
@@ -17,17 +17,18 @@ class Fitness:
 
 
 class AgentHandler:
-    def __init__(self, population: int):
+    def __init__(self, population: int, env_params: EnvParams):
         # Assign the previous best or generate new brain
         self.brain = Brain()
         self.population: int = population
         self.agents: list[Agent] = []
         self.best_agent: Agent | None = None
+        self.env_builder: EnvBuilder = EnvBuilder(env_params)
 
     def create_agents(self, brain: Brain, logger: Logger):
         if self.best_agent is not None:
             self.brain = self.best_agent.brain
         for id in range(self.population):
             self.agents.append(
-                Agent(id=id, brain=brain, env=env_builder.env, logger=logger)
+                Agent(id=id, brain=brain, env=env_builder.build(), logger=logger)
             )
