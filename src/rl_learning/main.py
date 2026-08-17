05020af0c585f867d3016bffaaaa6796dfe13951
Taylor <taylort3450@proton.me>
2026-08-17 13:32:39 -0500

    Create game instance with initial board positions

--- src/rl_learning/main.py
+++ src/rl_learning/main.py
@@ -1,5 +1,20 @@
+from rl_learning.game import Game, Position
+
+
 def main():
     # Step 1: Create Game
+    map_x_size = 5
+    map_y_size = 5
+    pit_pos = Position(x=1, y=5)
+    wumpus_pos = Position(x=0, y=0)
+    win_pos = Position(x=1, y=3)
+    Game(
+        x_size=map_x_size,
+        y_size=map_y_size,
+        pit_pos=pit_pos,
+        wumpus_pos=wumpus_pos,
+        win_pos=win_pos,
+    )
     # Step 2: Create Brain
     # Step 3: Create AgentHandler with Population and no Brain
     # Step 4: For each agent: set Agent brain
