0d1b1a3cb44e35dbbea6ee6239a57e327b3dd7e4
Taylor <taylort3450@proton.me>
2026-08-17 13:51:54 -0500

    Add brain mutation logic
    Initialize brain with random actions and mutation chance.
    Implement mutate_actions to append an action and randomly
    reassign existing actions based on mutation probability.

--- src/rl_learning/agent/brain.py
+++ src/rl_learning/agent/brain.py
@@ -1,3 +1,21 @@
+import random
+
+from rl_learning.game import Direction
+
+
 class Brain:
-    def __init__(self):
-        pass
+    def __init__(self, mutation_chance: float):
+        starting_action_count = 3
+        self.actions: list[Direction] = [
+            random.choice(list(Direction)) for _ in range(starting_action_count)
+        ]
+        self.mutation_chance: float
+
+    def mutate_actions(self, mutation_chance):
+        if mutation_chance > random.random():
+            return
+
+        self.actions.append(Direction.UP)
+
+        for action in range(len(self.actions)):
+            self.actions[action] = random.choice(list(Direction))
