8a84a6b019ca5974b3413193c9ebffded96a1251
Taylor <taylort3450@proton.me>
2026-08-17 13:51:50 -0500

    Add agent movement methods

--- src/rl_learning/agent/agent.py
+++ src/rl_learning/agent/agent.py
@@ -1,5 +1,5 @@
 from rl_learning.agent.brain import Brain
-from rl_learning.game import Game, Position
+from rl_learning.game import Direction, Game, Position
 
 
 class Agent:
@@ -7,3 +7,20 @@ class Agent:
         self.pos: Position = starting_pos
         self.brain: Brain = brain
         self.game: Game = game
+
+    def run_actions(self, actions: list[Direction]):
+        for direction in actions:
+            self.move(self.pos, direction)
+
+    def move(self, pos: Position, direction: Direction):
+        valid_directions = self.game.get_valid_moves(pos)
+        if direction in valid_directions:
+            match direction:
+                case Direction.LEFT:
+                    pos.x -= 1
+                case Direction.RIGHT:
+                    pos.x += 1
+                case Direction.UP:
+                    pos.y += 1
+                case Direction.DOWN:
+                    pos.y -= 1
