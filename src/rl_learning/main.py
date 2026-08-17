a0a9a5481c3ac383c414cb231f6ad74c74dc67c5
Taylor <taylort3450@proton.me>
2026-08-17 12:42:25 -0500

    Add evolutionary loop comments to main function

--- src/rl_learning/main.py
+++ src/rl_learning/main.py
@@ -1,4 +1,16 @@
 def main():
+    # Step 1: Create Game
+    # Step 2: Create Brain
+    # Step 3: Create AgentHandler with Population and no Brain
+    # Step 4: For each agent: set Agent brain
+    # Step 4: For each agent: run Agent actions
+    # Step 5: For each agent: get fitness score
+    # Step 6: If score > win_value in fitness: skip to Step 10. Else:
+    # Step 7: Extract Brain from best agent
+    # Step 8: Transplant best agent Brain into AgentHandler population
+    # Step 9: Jump to step 4
+    # Step 10:Output the best agent's path and score
+    # Step 11:Exit
     pass
 
 
