d276021230b4dceae98e850a15a31319e7086b28
Taylor <taylort3450@proton.me>
2026-08-17 22:51:21 -0500

    Add agent and brain classes

---
+++ src/dodge/agent/brain.py
@@ -0,0 +1,27 @@
+import random
+
+from dodge.control import DIRECTION_KEYS
+
+
+class Brain:
+    def __init__(self, mutation_chance: float):
+        starting_action_count = 1
+        self.actions: list[DIRECTION_KEYS] = [
+            random.choice(list(DIRECTION_KEYS)) for _ in range(starting_action_count)
+        ]
+        self.mutation_chance: float = mutation_chance
+
+    def mutate_actions(self):
+
+        for action in range(len(self.actions)):
+            if self.mutation_chance < random.random():
+                continue
+            self.actions[action] = self.get_random_move()
+        if self.mutation_chance > random.random():
+            self.add_random_move()
+
+    def add_random_move(self):
+        self.actions.append(self.get_random_move())
+
+    def get_random_move(self) -> DIRECTION_KEYS:
+        return random.choice(list(DIRECTION_KEYS))
