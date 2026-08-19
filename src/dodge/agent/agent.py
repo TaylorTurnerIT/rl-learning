a0ac33662e156b5969bab24c71909f9f865e7db2
Taylor <taylort3450@proton.me>
2026-08-19 14:04:30 -0500

    Refactor agent to use generic movement commands

--- src/dodge/agent/agent.py
+++ src/dodge/agent/agent.py
@@ -1,58 +1,37 @@
 from copy import deepcopy
+from enum import Enum
 
-from rl_learning.agent.brain import Brain
-from rl_learning.game import Direction, Game, Position, State
+from dodge.agent.brain import Brain
+from dodge.control import MovementCommand, parse_commands
+
+
+class State(Enum):
+    ALIVE = 0
+    DEAD = 1
 
 
 class Agent:
-    def __init__(self, starting_pos: Position, brain: Brain, game: Game):
-        self.starting_pos: Position = deepcopy(starting_pos)
-        self.pos: Position = starting_pos
+    def __init__(self, brain: Brain):
         self.brain: Brain = brain
-        self.game: Game = game
         self.state: State = State.ALIVE
 
     def run_actions(self):
+        parse_commands()
+        actions_to_run: list[MovementCommand] = []
+        actions_to_run.append(MovementCommand("x", 10))
+        actions_to_run.append(MovementCommand("neutral", 10))
         for direction in self.brain.actions:
-            self.move(self.pos, direction)
-            match self.game.check_pos(self.pos):
-                case State.DEAD:
-                    self.state = State.DEAD
-                    break
-                case State.WON:
-                    self.state = State.WON
-                    break
+            actions_to_run.append(MovementCommand(move=direction, duration_ms=100))
 
-    def move(self, pos: Position, direction: Direction):
-        valid_directions = self.game.get_valid_moves(pos)
-        if direction in valid_directions:
-            match direction:
-                case Direction.LEFT:
-                    pos.x -= 1
-                case Direction.RIGHT:
-                    pos.x += 1
-                case Direction.UP:
-                    pos.y += 1
-                case Direction.DOWN:
-                    pos.y -= 1
+    def step(self, direction: MovementCommand):
+        pass
 
-    def calculate_fitness(
-        self,
-    ) -> int:
+    def calculate_fitness(self) -> int:
         score: int = 0
         match self.state:
             case State.DEAD:
-                score -= 10000
-        score -= len(self.brain.actions) * 10
-
-        distance = self.game.calculate_distance(self.pos, self.game.win_pos)
-        starting_distance = self.game.calculate_distance(
-            self.starting_pos, self.game.win_pos
-        )
-        progress = starting_distance - distance
-        score += progress**2 * 100
+                score -= 100000
         return score
 
     def reset(self):
-        self.pos = deepcopy(self.starting_pos)
         self.state = State.ALIVE
