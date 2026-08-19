2f4c6feb3df2e14d5b433f68977cd6d14df73b0d
Taylor <taylort3450@proton.me>
2026-08-19 14:04:25 -0500

    Increase starting action count and add command parsing

--- src/dodge/agent/brain.py
+++ src/dodge/agent/brain.py
@@ -1,18 +1,20 @@
 import random
 
-from dodge.control import DIRECTION_KEYS
+from dodge.control import DIRECTION_KEYS, MovementCommand, parse_commands
+
+# MovementCommand("x", 50)
 
 
 class Brain:
     def __init__(self, mutation_chance: float):
-        starting_action_count = 1
-        self.actions: list[DIRECTION_KEYS] = [
+        starting_action_count = 3
+        self.actions: list[str] = [
             random.choice(list(DIRECTION_KEYS)) for _ in range(starting_action_count)
         ]
         self.mutation_chance: float = mutation_chance
+        parse_commands(self.actions)
 
     def mutate_actions(self):
-
         for action in range(len(self.actions)):
             if self.mutation_chance < random.random():
                 continue
@@ -23,5 +25,5 @@ class Brain:
     def add_random_move(self):
         self.actions.append(self.get_random_move())
 
-    def get_random_move(self) -> DIRECTION_KEYS:
+    def get_random_move(self) -> str:
         return random.choice(list(DIRECTION_KEYS))
