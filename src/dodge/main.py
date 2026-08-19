8794f663356746ab9cd19d814ab58ef56bbf98a5
Taylor <taylort3450@proton.me>
2026-08-19 14:04:08 -0500

    Initialize game with agents and population
    Implement main function to create starting brain, spawn 100 agents, and
    return exit code.

--- src/dodge/main.py
+++ src/dodge/main.py
@@ -1,5 +1,22 @@
+from dodge.agent.agent import Agent
+from dodge.agent.brain import Brain
+from dodge.control import control
+
+
 def main():
-    pass
+    # Initialize config
+    population: int = 100
+    mutation_chance: float = 0.1
+
+    # Create brain
+    starting_brain: Brain = Brain(mutation_chance)
+
+    # Create agents
+    agents: list[Agent] = []
+    for _ in range(population):
+        agents.append(Agent(brain=starting_brain))
+
+    return 0
 
 
 if __name__ == "__main__":
